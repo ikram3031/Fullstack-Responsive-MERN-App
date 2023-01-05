6003a2909477ea8dc84298aabce8ae3442458e96
ikram3031 <mdikram.3031@gmail.com>
2023-01-06 00:11:08 +0600

    Client Side Start

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+function App() {
+  return (
+    <div className="app">
+    </div>
+  );
+}
+
+export default App;
